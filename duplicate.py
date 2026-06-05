7c0a5f67796446a63b1f2a6cfcef67561626176a
Sinan-ck <mohammedsinan123ck@gmail.com>
2026-06-04 19:33:35 -0700

    Improve duplicate.py with better algorithm using sets

--- duplicate.py
+++ duplicate.py
@@ -1,10 +1,26 @@
-num = list(map(int,input().split()))
-ls=[]
-ls2=[]
-for i in num:
-    if i  in ls:
-        ls2.append(i)
-    else:
-        ls.append(i)
+"""
+Problem: Find Duplicate Numbers
+Given a list of integers, find all numbers that appear more than once.
 
-print(set(ls2))        
+Example:
+Input: [1, 2, 2, 3, 3, 3, 4]
+Output: {2, 3}
+
+Approach:
+- Use a set to track seen numbers (O(1) lookup)
+- When we see a number again, add it to duplicates set
+- Time: O(n), Space: O(n)
+"""
+
+num = list(map(int, input().split()))
+
+seen = set()
+duplicates = set()
+
+for i in num:
+    if i in seen:
+        duplicates.add(i)
+    else:
+        seen.add(i)
+
+print(duplicates)
